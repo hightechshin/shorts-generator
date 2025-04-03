be716399ad8e519ab70b231339e6d3bb3e80a17d
hightechshin <leesin8856@gmail.com>
2025-04-03 23:06:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -78,19 +78,21 @@ def upload_and_generate():
         drawtext_filters = []
         for sub in subtitles:
             alpha_expr = (
-                f"if(lt(t\,{sub['start']})\,0\," +
-                f"if(lt(t\,{sub['start']}+0.5)\,(t-{sub['start']})/0.5\," +
-                f"if(lt(t\,{sub['end']}-0.5)\,1\," +
-                f"(1-(t-{sub['end']}+0.5)/0.5)))"
-            )
+    f"if(lt(t\\,{sub['start']})\\,0\\,"
+    f"if(lt(t\\,{sub['start'] + 0.5})\\,(t-{sub['start']})/0.5\\,"
+    f"if(lt(t\\,{sub['end'] - 0.5})\\,1\\,"
+    f"(1-(t-{sub['end']}+0.5)/0.5)))"
+)
+
             drawtext = (
-                f"drawtext=fontfile='{font_path}':"
-                f"text='{sub['text']}':"
-                f"fontcolor=white:fontsize=60:x=(w-text_w)/2:y=(h-text_h)/2:"
-                f"alpha='{alpha_expr}':"
-                f"borderw=4:bordercolor=black:box=1:boxcolor=black@0.5:boxborderw=20:"
-                f"enable='between(t,{sub['start']},{sub['end']})'"
-            )
+    f"drawtext=fontfile='{font_path}':"
+    f"text='{sub['text']}':"
+    f"fontcolor=white:fontsize=60:x=(w-text_w)/2:y=(h-text_h)/2:"
+    f"alpha='{alpha_expr}':"
+    f"borderw=4:bordercolor=black:box=1:boxcolor=black@0.5:boxborderw=20:"
+    f"enable='between(t,{sub['start']},{sub['end']})'"
+)
+
             drawtext_filters.append(drawtext)
 
         filterchain = "scale=1080:1920," + ",".join(drawtext_filters)
