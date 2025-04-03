1de044d7f90ab6a0a607574acffe11bb0e9e3789
hightechshin <leesin8856@gmail.com>
2025-04-03 12:21:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -157,9 +157,10 @@ def upload_and_generate():
 def home():
     return "✅ Shorts Generator Flask 서버 실행 중"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 10000))
+#     app.run(host="0.0.0.0", port=port)
+
 
 
 
