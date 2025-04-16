5e5875b6a0418207a669102b9aabe412eca1ac0b
hightechshin <leesin8856@gmail.com>
2025-04-16 18:04:28 +0900

    Update route.py

--- refactored/logic/route.py
+++ refactored/logic/route.py
@@ -1,6 +1,7 @@
 from ..utils.haversine import haversine_km, estimate_travel_time_min
 from ..utils.naver_route import get_naver_driving_info
 from datetime import datetime
+from ..utils.logger import log
 
 def get_route_estimate(
     start_lat: float,
