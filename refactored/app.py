46cc8c59738789534a1482e0af17cc401c532416
hightechshin <leesin8856@gmail.com>
2025-04-16 09:07:49 +0900

    Update app.py

--- refactored/app.py
+++ refactored/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from .routes.video import video_bp
 from .utils.scheduler import start_scheduler  # ✅ 스케줄러 임포트
+from routes.weather import weather_bp
 
 app = Flask(__name__)
+
+
 app.register_blueprint(video_bp)
+app.register_blueprint(weather_bp)
+
 
 start_scheduler()  # ✅ 여기에서 한 번만 실행
 
