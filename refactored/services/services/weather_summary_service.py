f2abd9a71efecdce82a82180eb91077592b35151
hightechshin <leesin8856@gmail.com>
2025-04-16 09:04:17 +0900

    Create weather_summary_service.py

---
+++ refactored/services/services/weather_summary_service.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from utils.openmeteo_weather import get_openmeteo_forecast
+from utils.weather_summary_util import summarize_hourly_weather
+
+def generate_weather_summary(lat: float, lon: float, time_iso: str) -> str:
+    """
+    위도/경도 + ISO 시간 기반으로 날씨 요약문 생성
+    """
+    target_time = datetime.fromisoformat(time_iso)
+    forecast = get_openmeteo_forecast(lat, lon, target_time)
+    hourly_data = forecast.get("hourly", {})
+
+    summary = summarize_hourly_weather(hourly_data)
+    return summary
