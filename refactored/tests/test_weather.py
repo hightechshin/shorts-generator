5d736b91a4157fa2368765f5daca94a07c9ff8d4
hightechshin <leesin8856@gmail.com>
2025-04-16 18:27:11 +0900

    Update test_weather.py

--- refactored/tests/test_weather.py
+++ refactored/tests/test_weather.py
@@ -1,1 +1,11 @@
+from utils.openmeteo_weather import get_openmeteo_forecast
+
+def test_weather_fetch():
+    res = get_openmeteo_forecast(
+        37.57, 126.98, "2025-04-16T15:00"
+    )
+
+    assert "temperature" in res
+    assert "humidity" in res
+    assert "windspeed" in res
 
